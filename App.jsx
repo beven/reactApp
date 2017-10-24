1789c0e4e7f5bafab8095313a2352283c6b9e933
Matan Sar-Shalom <smatan84@gmail.com>
2017-10-24 15:55:09 +0300

    refactor components

--- App.jsx
+++ App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+import { Header } from "./Header.jsx";
+import { Content } from "./Content.jsx";
+
 document.body.style.backgroundColor = "steelblue";
 class App extends React.Component {
 	render() {
@@ -8,42 +11,6 @@ class App extends React.Component {
 			<div>
 				<Header/>
 				<Content/>
-			</div>
-		);
-	}
-}
-
-class Header extends React.Component {
-	render() {
-		return(
-			<div>
-				<h1 className="headerOne" style={{ color: '#27ae60', font: '70px Tahoma, Helvetica, Arial, Sans-Serif', textShadow: '0px 2px 3px rgb(114, 220, 159)' }}>Another boring '
-					<span class="amber">H</span>
-					<span class="pink">e</span>
-					<span class="yellow">l</span>
-					<span class="blue">l</span>
-					<span class="green">o</span>
-					<span class=""> </span>
-					<span class="lime">W</span>
-					<span class="grey">o</span>
-					<span class="deepOrange">r</span>
-					<span class="pink">l</span>
-					<span class="amber">d</span>
-					<span class="deepOrange">!</span>
-
-
-					' app!</h1>
-			</div>
-		);
-	}
-}
-
-class Content extends React.Component {
-	render(){
-		return(
-			<div id="initialDiv">
-				<h2 className="headerTwo">Can we improve it?</h2>
-				<p>Maybe we can together ...</p>
 			</div>
 		);
 	}
---
+++ Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Content extends React.Component {
+	render(){
+		return(
+			<div id="initialDiv">
+				<h2 className="headerTwo">Can we improve it?</h2>
+				<p>Maybe we can together ...</p>
+			</div>
+		);
+	}
+}
